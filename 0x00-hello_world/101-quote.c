40f28cc96125cd5664bd9465634d25de2b1e837c
ch-canaza <ch-canaza@hotmail.com>
2020-02-06 16:45:12 +0000

    DORA KORPAR

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * main - print a mesage
+ * write - write to a file descriptor
+ * function: main
+ * Description : error
+(*
+ * Return: print text
+*/
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n");
+	return (1);
+}
