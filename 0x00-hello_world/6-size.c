95d1a5e740053f1ce77e95ddfa3a110b1b6d2849
ch-canaza <ch-canaza@hotmail.com>
2020-02-06 03:42:57 +0000

    printing sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+ *main - print function
+ *Return:0
+ */
+int main(void)
+{
+	printf("size of a char %d/n"), sizeof(char)
+	printf("size of a int %d/n"), sizeof(int)
+	printf("size of a long int %d/n"), sizeof(long)
+	printf("size of a long long %d/n"), sizeof(long - long)
+	printf("size of float %d/n"), sizeof(float);
+return (0);
+}
