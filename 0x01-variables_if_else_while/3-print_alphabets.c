5cdf7226eeff3aa796293e3458dcc02a9ecc53dc
ch-canaza <ch-canaza@hotmail.com>
2020-02-07 17:51:26 +0000

    alphabet upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,16 +13,19 @@ int main(void)
 
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-{
+
 
 	putchar(ch);
 
-}
+
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
 
-{
+
 	putchar(ch);
-}
+
+
+	putchar ('\n');
+
 	return (0);
 
 }
