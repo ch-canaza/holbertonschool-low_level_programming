bcbbfde76b6c4584dd08c3902da0ea122f9b3c91
ch-canaza <ch-canaza@hotmail.com>
2020-02-07 03:47:12 +0000

    number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,19 @@
 #include <time.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * Return: always 0 (succes)
  */
-int main(void)
+
+	int main(void)
+
 {
 	int x;
 
 	for (x = 0; x < 10; x++)
 	putchar(48 + x);
 	putchar(10);
-	return (0);
+	return(0);
 }
