9eb80ec1bed735ba1aa6cef9777ded2bd1e45bc5
ch-canaza <ch-canaza@hotmail.com>
2020-02-07 04:06:22 +0000

    lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Desc: Entry
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 122 ; ch >= 97 ; --ch)
+	{
+	putchar(ch);
+
+	}
+
+	putchar ('\n');
+	return (0);
+}
