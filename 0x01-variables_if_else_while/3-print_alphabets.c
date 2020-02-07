1b89f8860d39bd608e75f092c1a25d3728b48b00
ch-canaza <ch-canaza@hotmail.com>
2020-02-07 01:27:51 +0000

    alphabet lower upp

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ * Description: entry
+ *Return: alphabet
+ */
+int maun(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+
+	putchar(ch);
+
+}
+	for (char = A  ch <= Z ch++)
+
+{
+	putchar(ch);
+	
+	return (0);
+}
