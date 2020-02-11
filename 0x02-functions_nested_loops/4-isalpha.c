fbe205407d7829b6d2298d243860df0603cbc3f1
ch-canaza <ch-canaza@hotmail.com>
2020-02-11 16:21:20 +0000

    check alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *  _isalpha - print alphabet
+ * @c: alphabet
+ * Return: 0
+ */
+int _isalpha(int c)
+
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c 
+<= 90))
+	{
+		if (c != 59)
+		return (1);
+	}
+	return (0);
+
+
+}
