b5ab5d3a486ab215f483b43c73bfb8c8e5fb692a
ch-canaza <ch-canaza@hotmail.com>
2020-02-11 15:22:20 +0000

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 {
 	int x;
 	int i;
- 
+
 	for (i = 0 ; i < 10 ; i++)
-	{	
-	
-		for (x = 'a' ; x <= 'z' ; x++)
-			_putchar(x);
-		_putchar('\n');
+	{
+
+	for (x = 'a' ; x <= 'z' ; x++)
+		_putchar(x);
+	_putchar('\n');
 	}
 }
