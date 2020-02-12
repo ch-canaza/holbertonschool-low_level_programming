27075990caa27969c21228104f2c41ab7876282a
ch-canaza <ch-canaza@hotmail.com>
2020-02-12 00:48:45 +0000

    addin numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *add - addition
+ *@x: the integer received
+ *@y:  the integer received
+ *Return: x
+ */
+int add(int x, int y)
+{
+	int m = x + y;
+
+	return (m);
+}
+
