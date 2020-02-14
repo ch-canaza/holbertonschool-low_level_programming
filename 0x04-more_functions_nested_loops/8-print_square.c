b94c9aab982cd6e9f95230f95a6b5287ab7ca817
ch-canaza <ch-canaza@hotmail.com>
2020-02-14 03:46:02 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+
+		for (x = 0; x < size; x++)
+	{
+		for(y = 0; y < size; y++)
+
+			putchar('#');
+	}
+	_putchar('\n');
+	}
+}
+else
+{
+	_putchar('\n');
+}
+}
