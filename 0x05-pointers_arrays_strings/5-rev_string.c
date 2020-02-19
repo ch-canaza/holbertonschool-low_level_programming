e6606bdb6a6db001a56f514d8d915a592bdd6ac9
ch-canaza <ch-canaza@hotmail.com>
2020-02-19 01:46:14 +0000

    reverses

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * rev_string - fuction that reverse a string
+ * @s: pointer to a string
+ *
+ */
+
+void rev_string(char *s)
+
+{
+	int i;
+	int j;
+	char tmp; 
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+
+	}
+i--;
+
+	for (j = 0; j < i; j++,i--)
+	{
+
+	tmp = *(s + i);
+	*(s + i) = *(s + j);
+	*(s + j) = tmp;
+	}
+
+
+}
