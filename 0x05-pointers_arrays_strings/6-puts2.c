e999adbbe80d8b1fdb680e9719d3541f48a6ddd7
ch-canaza <ch-canaza@hotmail.com>
2020-02-19 02:12:10 +0000

    pair numbers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *puts2 - print every single character in a string
+ *@str: string stored
+ *
+ */
+
+void puts2(char *str)
+
+{
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+
+		_putchar(str[i]);
+	}
+_putchar('\n');
+
+}
