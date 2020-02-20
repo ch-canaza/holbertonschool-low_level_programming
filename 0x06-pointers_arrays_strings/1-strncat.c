179c81d7cf7b9f5ba7a68bec2504b0011eb20969
ch-canaza <ch-canaza@hotmail.com>
2020-02-20 17:50:05 -0500

    sdding a byte

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenate strings
+ *@n: quantity of spaces
+ * @dest: string that locate src
+ * @src: string that is pasted in desc
+ *
+ * Return:dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int i = 0, j, k = 0;
+
+	while (src[k] != '\0')
+		k++;
+	if (n > (k - 1))
+		n = k;
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+
+       }
+	return(dest);
+}
