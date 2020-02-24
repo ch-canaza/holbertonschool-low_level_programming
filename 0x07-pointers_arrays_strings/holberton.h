fd76288999847d705eaa39c379fa754980a12b90
ch-canaza <ch-canaza@hotmail.com>
2020-02-24 16:07:16 -0500

    mem copy

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -27,4 +27,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif /* HOLBERTON_H */
