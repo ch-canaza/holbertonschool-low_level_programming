e45cd660720a350667633964523d55fdccc018c1
ch-canaza <ch-canaza@hotmail.com>
2020-02-25 16:32:59 -0500

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square int matrix
+ * @a: input matrix
+ * @size: size of the matrix
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int j;
+	int pos;
+	int diag1 = 0;
+	int diag2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			pos = (i * size) + j;
+			if (i == j)
+				diag1 += a[pos];
+
+			if ((i + j) == (size - 1))
+				diag2 += a[pos];
+		}
+	}
+
+	printf("%d, %d\n", diag1, diag2);
+}
