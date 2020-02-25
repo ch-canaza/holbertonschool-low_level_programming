b0f93681eb9a28b8fdf4da605e69f6110afa5741
ch-canaza <ch-canaza@hotmail.com>
2020-02-25 16:22:33 -0500

    locate a substring

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -33,4 +33,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
 #endif /* HOLBERTON_H */
