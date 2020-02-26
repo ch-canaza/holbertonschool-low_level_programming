bba5d2e05072a77d30a0274be1602aae4f4cfa4a
ch-canaza <ch-canaza@hotmail.com>
2020-02-26 16:27:13 -0500

    printing strings

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,39 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
+void _puts_recursion(char *s);
+#endif /* HOLBERTON_H */
