85c64d3770d89d14a95e112c71e1bc07c84d5166
ch-canaza <ch-canaza@hotmail.com>
2020-02-29 18:01:23 -0500

    my name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- * print_name - prints it name
- * return: 0
- * argv: type of argument
- * argc: number of argument
- * main: contain funtion
+ * main - print the name of the program
+ * @argc: count the number of arguments passed to the program
+ * @argv: array that contains the arguments of the program
+ * Return: always 0
  */
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	(void)argc;
-
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
