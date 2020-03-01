c17b1ec52464defacfe242fa413a8a2d6369384f
ch-canaza <ch-canaza@hotmail.com>
2020-02-29 19:00:05 -0500

    printing arguments one per line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: count the number of arguments
+ * @argv: array with the arguments of the program
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", (argv[i]));
+	}
+	return (0);
+}
