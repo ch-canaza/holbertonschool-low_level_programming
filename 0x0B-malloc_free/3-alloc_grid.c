f45b23686024d4ea9e5a8745d9febf4fd06f1736
ch-canaza <ch-canaza@hotmail.com>
2020-03-04 19:21:51 -0500

    allocate initialized grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - alloc_grid
+ * @width: width
+ * @height: height
+ * Return: double pointer.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, x;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+
+	p = malloc(sizeof(p) * height);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+
+		if (p[i] == NULL)
+		{
+			for (x = i; x >= 0; x--)
+				free(p[x]);
+			free(p);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (x = 0; x < width; x++)
+			p[i][x] = 0;
+	}
+
+	return (p);
+}
