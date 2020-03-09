03fe3f23535769f39ca51c1d4ffa423a6c87ee7a
ch-canaza <ch-canaza@hotmail.com>
2020-03-09 17:15:35 -0500

    dog structture

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog-structure
+ * @name: name of the instance
+ * @age: age of the instance
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
