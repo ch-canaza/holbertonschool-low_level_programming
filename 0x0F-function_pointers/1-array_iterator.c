6029fb42fcba9f3448b6fdf0dbfbf86465d7291c
ch-canaza <ch-canaza@hotmail.com>
2020-03-11 14:53:53 -0500

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,10 +14,13 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 	i = 0;
 
-		while (i < size)
+	if (size <= 0 || array == NULL || action == NULL)
+		return;
+
+	while (i < size)
 		{
 
-		     (action) (array[i++]);
+		     action(array[i++]);
 
 		}
 }
