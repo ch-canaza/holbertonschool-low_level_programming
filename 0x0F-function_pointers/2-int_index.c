4086b8e4003740c416d222131203617409855f9a
ch-canaza <ch-canaza@hotmail.com>
2020-03-11 16:06:09 -0500

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * int_index - function that prints names
+ * @size: pointer
+ * @cmp: pointer to name
+ * @array: array of pointers
+ * Return: always zero
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+
+	}
+	return (-1);
+}
