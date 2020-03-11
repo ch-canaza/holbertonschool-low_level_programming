2e38eddf4461ccbadbd3bce446fc6b138843677d
ch-canaza <ch-canaza@hotmail.com>
2020-03-11 15:00:55 -0500

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,21 +6,23 @@
  * array_iterator - function that prints names
  * @size: pointer
  * @action: pointer to name
+ * @array: array of pointers
  * Return: always zero
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
 	i = 0;
 
 	if (size <= 0 || array == NULL || action == NULL)
 		return;
 
 	while (i < size)
-		{
+	{
 
-		     action(array[i++]);
+		action(array[i++]);
 
-		}
+	}
 }
