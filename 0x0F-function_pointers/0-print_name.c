cf9a660581d9aa11734d1a4bf4cd5d15158d7d1d
ch-canaza <ch-canaza@hotmail.com>
2020-03-11 14:43:07 -0500

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include "holberton.h"
-#include "function_pointers.h"
 
 /**
  * print_name - function that prints names
@@ -13,6 +11,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-
 	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#ifndef FUNCTION_POINTER_H
-#define  FUNCTION_POINTER_H
-
+#ifndef FPOINTER_H
+#define FPOINTER_H
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
