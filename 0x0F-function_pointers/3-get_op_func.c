ca2cc584179c97caac3ef03490a3cd20e66377ab
ch-canaza <ch-canaza@hotmail.com>
2020-03-12 16:57:16 -0500

    gettin operators

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,35 +1,33 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * get_op_func - select function
- *@s:int symbol
- *Return: pointer to function
- */
+#include "3-calc.h"
 
+/**
+ * get_op_func - Select the correct function to perform the operation
+ * @s: The operator passed as argument to the program
+ *
+ * Return: Pointer to the function that corresponds to the operator
+ */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i= 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    while (*s != *ops[i].op && i < 4)
-    {
-	    i++;
+	i = 0;
+	while (ops[i].op)
+	{
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
-    }
-    if (s == NULL || s[2] == '\0' || *s != *ops[i].op)
-	    {
-		    return(NULL);
-   	    }
-	    return (*ops[i].f);
-	    
-	    /* printf("EROR\n");
-	       exit (99);*/
+	printf("Error\n");
+	exit(99);
 }
