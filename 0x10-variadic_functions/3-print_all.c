6033ee63714ed41457590f849dbac82dd5ad340d
ch-canaza <ch-canaza@hotmail.com>
2020-03-14 13:10:06 -0500

    printing anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,77 +1,53 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
- * print_chars - prints characters
- * @str_numbers: parameter
- * Return: void
- */
-void print_chars(va_list str_numbers)
-{
-	printf("%c", va_arg(str_numbers, int));
-}
-/**
- * print_integers - prints integers
- * @str_numbers: parameter
- * Return: void
- */
-void print_integers(va_list str_numbers)
-{
-	printf("%d", va_arg(str_numbers, int));
-}
-/**
- * print_floats - prints floats
- * @str_numbers: parameter
- * Return: void
- */
-void print_floats(va_list str_numbers)
-{
-	printf("%f", va_arg(str_numbers, double));
-}
-/**
- * print_string - prints strings
- * @str_numbers: parameter
- * @str_numbers: parameter
- */
-void print_string(va_list str_numbers)
-{
-	printf("%s", va_arg(str_numbers, char *));
-}
-/**
- * print_all - prints anything
- * @format: constant pointer
- * Return: void
+ * print_all - Entry point
+ * Desc: Entry
+ *@format: value
+ * Return: Always 0 (Success)
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	int j;
-	va_list vadlis;
+	char *k;
+	va_list valist;
+	unsigned int i, j;
 
-	f_t fmt_s[] = {
-		{"c", print_chars},
-		{"i", print_integers},
-		{"f", print_floats},
-		{"s", print_string},
-		{NULL, NULL}
-	};
-
-	va_start(vadlis, format);
-
-	while (format[i] != '\0' && format)
+	i = 0;
+	while (format != NULL)
 	{
-		j = 0;
-		while (fmt_s[j].symbol != NULL)
+		va_start(valist, format);
+		while (format[i] != 0)
 		{
-			if (fmt_s[j].symbol[0] == format[i])
+			j = 1;
+			switch (format[i])
 			{
-				fmt_s[j].p(vadlis); /*llamada a la funcion*/
-				if (fmt_s[j + 1].symbol != NULL)
-					printf(", ");
+				case 'c':
+				printf("%c", va_arg(valist, int));
+				break;
+				case 'i':
+				printf("%d", va_arg(valist, int));
+				break;
+				case 'f':
+				printf("%f", va_arg(valist, double));
+				break;
+				case 's':
+				k = va_arg(valist, char *);
+				if (k == 0)
+				k = "(nil)";
+				printf("%s", k);
+				break;
+				default:
+				j = 0;
 				break;
 			}
-			j++;
+			if (format[i + 1] && j)
+				printf(", ");
+			i++;
 		}
-		i++;
+		va_end(valist);
+		break;
 	}
 	printf("\n");
-	va_end(vadlis);
 }
