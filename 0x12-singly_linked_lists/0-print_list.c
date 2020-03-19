219557da11a4072f5c9a2ed565ccf383a0de2150
ch-canaza <ch-canaza@hotmail.com>
2020-03-19 15:26:46 -0500

    printin list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *print_list- print the list in a struct
+ *@list_t:list with elements to be printed
+ *@h: pointer to head of list
+ *Return:zero
+ */
+
+
+size_t print_list(const list_t *h)
+
+{
+
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		if (h -> str == NULL)
+
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n",h -> len, h -> str);
+
+			       h = h ->next;
+			       size++;
+
+	}
+
+	return (size);
+}
