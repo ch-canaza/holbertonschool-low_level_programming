c8d483d3c08d3b1ab6f6206d23f69ac2a15313a8
ch-canaza <ch-canaza@hotmail.com>
2020-03-19 19:10:33 -0500

    fix adding new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,18 +10,46 @@
  */
 
 list_t *add_node(list_t **head, const char *str)
-
 {
 	list_t *new_node;
 
+	if (!head || !str)
+		return (NULL);
+
 	new_node = malloc(sizeof(list_t));
 
-	while (new_node != NULL)
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (!new_node->str)
 	{
-		new_node->str = strdup(str);
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		free(new_node);
+		return (NULL);
 	}
-	return (NULL);
+
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+
+
+}
+
+/**
+ * _strlen - Return the length of a string
+ * @s: The string to be checked
+ *
+ * Return: The length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
 }
