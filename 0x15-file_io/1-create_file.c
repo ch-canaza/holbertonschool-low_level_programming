e71a055f7c4f6e3d66305ab6721b382ed480e019
ch-canaza <ch-canaza@hotmail.com>
2020-03-31 15:42:03 -0500

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * create_file - check the code for Holberton School students.
+ *@filename: pointer to name of the file
+ *@text_content: 1 on succes -1 on failure
+ * Return:the actual number of letters it could read and print
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, w, i;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC,0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	return (1);
+
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		w = write(file, text_content, i);
+
+	}
+		if (w == -1)
+			return (-1);
+
+		close(file);
+		return (1);
+
+}
