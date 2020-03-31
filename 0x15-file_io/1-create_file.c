59683f6b582457d2e3987f5d79b22e27d0d9a53d
ch-canaza <ch-canaza@hotmail.com>
2020-03-31 17:39:50 -0500

    quiting return

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,12 +15,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR, S_IWUSR);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC | S_IRUSR, S_IWUSR);
 	if (file == -1)
 	{
 		return (-1);
 	}
-	return (1);
+	/*return (1);*/
 
 	if (text_content)
 	{
@@ -30,10 +30,10 @@ int create_file(const char *filename, char *text_content)
 
 	}
 	if (w == -1 || w != i)
-		{
-			close(file);
-			return (-1);
-		}
+	{
+		close(file);
+		return (-1);
+	}
 
 		close(file);
 		return (1);
