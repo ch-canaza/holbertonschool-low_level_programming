0f53b57bfbf05368aa9472eef1652b12633d6202
ch-canaza <ch-canaza@hotmail.com>
2020-03-31 17:02:19 -0500

    append text in a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - check the code for Holberton School students.
+ *@filename: pointer to name of the file
+ *@text_content: 1 on succes -1 on failure
+ * Return:1 on succes -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, app, i = 0;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	return (1);
+
+	if (text_content)
+	{
+		while (text_content[i] != '\0')
+			i++;
+		app = write(file, text_content, i);
+
+	}
+	if (app == -1 || app != i)
+		{
+			close(file);
+			return (-1);
+		}
+
+		close(file);
+		return (1);
+
+}
