3139fd2807b432a2f417c9feea0d8b8c8b3de25a
ch-canaza <ch-canaza@hotmail.com>
2020-04-01 20:26:23 -0500

    fixing 3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,43 +8,41 @@
 
 int main(int argc, char *argv[])
 {
-	char buf[1024];
-	int file_from, file_to, r, w;
+	 int fsrc, fdst, r, cl1, cl2;
+	 char buf[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "usage: cp file_from file_to\n");
-			exit(97);
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	 fsrc = open(argv[1], O_RDONLY);
+	if (fsrc == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-
-	dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-	exit(99);
-
-	do {
-
-		r = read(file_from, buf, 1024);
-		w = write(file_to, buf, r);
-	}
-	while (r == 1024)
-		if (r == -1)
+	fdst = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	while ((r = read(fsrc, buf, 1024)) > 0)
+	{
+		if (fdst == -1 || (write(fdst, buf, r) != r))
 		{
-
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-			exit(98);
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+		    exit(99);
+	}
+	if (r == -1)
+	{	dprintf(STDERR_FILENO, "Error: can't read from %s\n", argv[2]);
+		exit(98);
+	}
+		cl1 = close(fsrc);
+		if (cl1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't close%d\n", fsrc);
+			exit(100);
 		}
-	if (w <= -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file_from), exit(100);
-	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", file_to), exit(100);
-	return (0);
-
-
+		cl2 = close(fdst);
+		if (cl2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't close%d\n", fdst);
+			exit(100);
+		}
+		return (0);
+}
 }
