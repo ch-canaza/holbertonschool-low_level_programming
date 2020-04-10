1a817f21acc2c14811ed27e2f998d03d2c96e21e
ch-canaza <ch-canaza@hotmail.com>
2020-04-10 11:16:26 -0400

    task 3 missing handle errors

--- testing.c
+++ testing.c
@@ -1,48 +1,77 @@
-#include <sys/types.h>
-#include "header.h"
+#include <sys/stat.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int ac, char **ar)
+//#include "header.h"
+int main(int ac, char **ar, char **envp)
 {
-
-	char **env = __environ;
-	//char **ar;
-	char *buffer = NULL;
-	char *av = NULL;
-	char *tok = NULL;
-	int bufsize = 100;
-	int pos = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
-	size_t size = 0;
-	pid_t pid;
-	int rex = 0;
-	int stat;
-	int g; 
-	
-	printf("#cisfun$ ");
-	getline(&buffer, &size, stdin);
-	tok = strtok(buffer, "\t\n\a\r ");
-//////////////////////////////////////
-/*	 while (tokens != NULL) {*/
-	tokens[pos] = tok;
-   	 pos++;
-	// }*/
-/////////////////////////////////////////	 
-	pid = fork();
-	if (pid == 0)
-		ar = tokens;
-		tok = strtok(NULL, " ");
-		if (execve(ar[0], ar, NULL) == -1)
-			perror("./shell");
-	
-	do {	
-				
-		wait(&stat);
-	}while (stat);
-	tokens[pos] = NULL;
-	free(buffer);
-	return (EXIT_SUCCESS);
+        char **env = __environ;
+        //char **ar;
+        char *buffer = NULL;
+        char *av[10];
+        char *tok = NULL;
+        //char *tok2[100];
+        //
+        int bufsize = 100;
+        int pos = 0;
+        // char **tokens = malloc(bufsize * sizeof(char));
+        //char **split = malloc(bufsize * sizeof(char));
+        size_t size = 0;
+        pid_t pid;
+        int rex = 0;
+        int status;
+        int g;
+        int i = 0, j;
+        struct stat st;
+    do{
+        pos = 0;
+        if(isatty(STDIN_FILENO))
+            printf("#cisfun$ ");
+        g = getline(&buffer, &size, stdin);
+        if (g == EOF){
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            free(buffer);
+            exit(0);
+        }
+        if (strcmp(buffer, "exit\n") == 0)
+                exit(0);
+        // tokens inicia aqui
+        tok = strtok(buffer, "\t\n\a\r ");
+        ///
+        while (tok != NULL)
+        {
+            av[pos] = tok;
+            pos++;
+            tok = strtok(NULL, " \t\n\r");
+        }
+        av[pos] = NULL;
+        ///     termina aqui*/
+        // if (stat(ar[0], &st) == 0)
+        // tokens[pos] = tok;
+        //if ((*buffer != '\n') && (*buffer != ' '))
+        if ((*buffer != '\n') && av[0])    
+        {    pid = fork();
+            if (pid == 0)
+            {       
+                if (execve(av[0], av, envp) == -1) 
+                        perror("./shell");
+            }
+            else
+                wait(&status);
+        }
+        pos = 0;
+        while (pos < 10)
+        {
+            av[pos] = 0;
+            pos++;
+        }
+    } while (1);
+        av[pos] = NULL;
+        free(buffer);
+        free(av);
+        //free(split);
+         return (EXIT_SUCCESS);
 }
