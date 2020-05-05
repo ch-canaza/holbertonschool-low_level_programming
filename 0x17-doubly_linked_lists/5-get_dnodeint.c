b24ed837509078f91cc966debf5def571f3e3184
ch-canaza <ch-canaza@hotmail.com>
2020-05-04 19:18:30 -0500

    fixing edge case

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,11 +13,11 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *tmp = head;
 
-	if (head )
+	if (head)
 	{
 		if (tmp == NULL)
 		{
-			return(NULL);
+			return (NULL);
 		}
 		while (tmp != NULL && i < index)
 		{
