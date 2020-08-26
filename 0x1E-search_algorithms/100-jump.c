4deb5f0dbda6b56a4b5b861b25be8eef40c22d06
ch-canaza <ch-canaza@hotmail.com>
2020-08-26 13:44:40 -0500

    betty styles

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -6,7 +6,7 @@
  * @array: is a pointer to the first element of the array to search in
  * @size: is the number of elements in array
  * @value: is the value to search for
- * return: -1 if no value or array is NULL
+ * Return: -1 if no value or array is NULL
  * otherwise the index where value is located
  */
 
@@ -27,12 +27,12 @@ int jump_search(int *array, size_t size, int value)
 
 	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	if (jump < size -1)
+	if (jump < size - 1)
 	{
 		jump = jump;
 	}
-	jump = size -1;
-	
+	jump = size - 1;
+
 	for (; i < jump && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
